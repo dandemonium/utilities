f3e68fef0f63b01e77e1cad2da54e39a5ce29cbe
Daniel J. Stevens <6106495+dandemonium@users.noreply.github.com>
2026-01-09 16:28:38 -0600

    Add SPHEREx-to-EXOFASTv2 Spectrophot Converter
    Add Python script that converts table of SPHEREx spectrophotometry (wavelength and width in microns, then flux density and error in micro-Jansky) to EXOFASTv2-supported format of wavelength (microns) and flux and error (erg/s/cm^2).

---
+++ spherex_flux_jy2lamflam.py
@@ -0,0 +1,26 @@
+"""Convert SPHEREx spectrophotometry from IRSA:
+	lam(um)	lam_width(um)	Flux(uJy)	Flux_err(uJy)
+	into format expected by EXOFASTv2:
+	lam(um)	Flux(cgs)	Flux_err(cgs)
+"""
+
+import numpy as np, sys
+
+def convert_to_lamflam(lam, f_ujy):
+	""" Converts input flux F in micro-Janksy
+		to F_lambda, then to lambda*F_lambda,
+		where lambda is in microns.
+		Formula from Appendix 23 of kaSTScI's
+		NICMOS Instrument Handbook for Cycle
+		11, version 5:
+		flam = beta * fnu / lam"""
+
+	beta = 3e-9 #to get erg/s/cm^2/um
+	return np.float64((beta*f_ujy*1e-6)/lam) #/(lam**2.)
+	
+fname = sys.argv[1]
+
+lam, width, flux_ujy, flux_err_ujy = np.loadtxt(fname, unpack=True, comments='#')
+f_cgs = convert_to_lamflam(lam, flux_ujy)
+f_err_cgs = convert_to_lamflam(lam, flux_err_ujy)
+np.savetxt("test.csv", np.array([lam,f_cgs,f_err_cgs]).transpose(), fmt=('%.8f','%.8e','%.8e'), delimiter='\t')#overwrite=True)
