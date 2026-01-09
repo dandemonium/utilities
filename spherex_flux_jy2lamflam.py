5ece65059de3637b0e17e2153256a9ed4e154256
Daniel J. Stevens <6106495+dandemonium@users.noreply.github.com>
2026-01-09 16:33:11 -0600

    Update spherex_flux_jy2lamflam.py
    add second input argument for star name, so output file name follows convention from EXOFASTv2's mkticsed.pro

--- spherex_flux_jy2lamflam.py
+++ spherex_flux_jy2lamflam.py
@@ -16,11 +16,11 @@ def convert_to_lamflam(lam, f_ujy):
 		flam = beta * fnu / lam"""
 
 	beta = 3e-9 #to get erg/s/cm^2/um
-	return np.float64((beta*f_ujy*1e-6)/lam) #/(lam**2.)
+	return np.float64((beta*f_ujy*1e-6)/lam)
 	
 fname = sys.argv[1]
-
+star = sys.argv[2]
 lam, width, flux_ujy, flux_err_ujy = np.loadtxt(fname, unpack=True, comments='#')
 f_cgs = convert_to_lamflam(lam, flux_ujy)
 f_err_cgs = convert_to_lamflam(lam, flux_err_ujy)
-np.savetxt("test.csv", np.array([lam,f_cgs,f_err_cgs]).transpose(), fmt=('%.8f','%.8e','%.8e'), delimiter='\t')#overwrite=True)
+np.savetxt(star+".SPHEREx.sed", np.array([lam,f_cgs,f_err_cgs]).transpose(), fmt=('%.8f','%.8e','%.8e'), delimiter='\t')
